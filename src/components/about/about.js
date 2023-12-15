dd531aebb92862f56a61f2b90d02dade961273c6
baidush <kostixaid@gmail.com>
2023-12-14 23:57:05 -0800

    fix: minor

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export default styled(({ title = 'about', text = '' }) => {
   return (
     <div>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <p className="about" dangerouslySetInnerHTML={{ __html: text }}></p>
     </div>
   )
